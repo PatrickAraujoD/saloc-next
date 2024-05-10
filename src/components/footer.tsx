995582ba90c8da7ef1ef8ac04ac93b41a1109237
PatrickAraujoD <135553025+PatrickAraujoD@users.noreply.github.com>
2024-05-10 12:53:14 -0300

    feat: add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export function Footer() {
+  return (
+    <footer className="bg-blue-900 h-28 w-full flex flex-col px-6 items-start justify-center text-white md:px-20">
+      <p className="uppercase font-bold text-xs md:text-base">
+        saloc - sistema de alocação de salas
+      </p>
+      <p className="uppercase font-bold text-xs md:text-base">
+        ccet - centro de ciência exatas e tegnologias
+      </p>
+    </footer>
+  )
+}

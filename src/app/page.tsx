9d0b129d7f04da9273086dbf774e5d8f980c065d
PatrickAraujoD <135553025+PatrickAraujoD@users.noreply.github.com>
2024-05-10 13:28:07 -0300

    refactor: swtich to page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import { Footer } from '@/components/footer'
+import { Header } from '@/components/header'
+import { Select } from '@/components/select'
+
 export default function Home() {
-  return <h1>Hello word</h1>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="px-6 md:px-10 flex-grow">
+        <section></section>
+        <form action="">
+          <Select name="course" nameLabel="curso" />
+          <Select name="period" nameLabel="periodo" />
+          <Select name="teacher" nameLabel="docente" />
+          <Select name="discipline" nameLabel="disciplina" />
+          <button
+            type="submit"
+            className="w-full bg-blue-900 h-10 text-white uppercase font-bold mt-4 rounded-md hover:bg-blue-950 hover:ease-in duration-200"
+          >
+            consultar
+          </button>
+        </form>
+      </main>
+      <Footer />
+    </div>
+  )
 }

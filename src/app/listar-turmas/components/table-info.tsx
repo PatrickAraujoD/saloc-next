c27867333a67b2d7d440f43df95fb76fd997e5b1
PatrickAraujoD <lordpatrick@gmail.com>
2024-06-04 08:46:37 -0300

    feat: add TableInfo component

--- src/app/listar-turmas/components/table-info.tsx
+++ src/app/listar-turmas/components/table-info.tsx
@@ -1,15 +1,15 @@
-import { Table } from '@/components/table'
+import { ClassroomList } from '@/components/classroom-list'
 
 export function TableInfo() {
   return (
     <main className="flex-grow py-10 px-2 xl:px-16">
       <section>
         <h2 className="uppercase font-semibold mb-4">turmas sem salas</h2>
-        <Table acao={true} />
+        <ClassroomList acao={true} />
       </section>
       <section>
         <h2 className="uppercase font-semibold mb-4">turmas com salas</h2>
-        <Table acao={true} />
+        <ClassroomList acao={true} />
       </section>
     </main>
   )

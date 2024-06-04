d8806ae249281a11a086053ec4d2ed62d6a46508
PatrickAraujoD <lordpatrick@gmail.com>
2024-06-04 08:45:13 -0300

    refactor: change name alocarSala to ListarTurmas

--- src/app/listar-turmas/page.tsx
+++ src/app/listar-turmas/page.tsx
@@ -5,7 +5,7 @@ import { getServerSession } from 'next-auth'
 import { nextAuthConfig } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 
-export default async function AlocarSala() {
+export default async function ListarTurmas() {
   const session = await getServerSession(nextAuthConfig)
 
   if (!session) return redirect('/login')

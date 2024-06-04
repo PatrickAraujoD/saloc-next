f042c7738b9ca00f9352489324c3daabd21f4173
PatrickAraujoD <lordpatrick@gmail.com>
2024-06-04 11:17:40 -0300

    style: add size for columns

--- src/components/td.tsx
+++ src/components/td.tsx
@@ -3,5 +3,9 @@ interface TdProps {
 }
 
 export function Td({ content }: TdProps) {
-  return <td className="border-2 border-black text-xs xl:text-sm">{content}</td>
+  return (
+    <td className="border-2 border-black text-xs xl:text-sm w-56 text-center">
+      {content}
+    </td>
+  )
 }

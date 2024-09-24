3553f3dad6dbc97f1db866a941925a8756eff516
PatrickAraujoD <lordpatrick200@gmail.com>
2024-09-24 10:27:39 -0300

    refactor:  move router.push to useEffect for client-side safety

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 'use client'
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function Page() {
   const router = useRouter()
 
-  return router.push('/home')
+  useEffect(() => {
+    router.push('/home')
+  })
+
+  return <h1 className="flex-1">Carregando...</h1>
 }

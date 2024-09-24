792a86096939841d8d6e2b9285518f204d6790d6
PatrickAraujoD <lordpatrick200@gmail.com>
2024-09-24 20:18:55 -0300

    refactor: change to console.log in api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,8 @@ import { config } from 'dotenv'
 
 config()
 
+console.log(process.env.NEXT_PUBLIC_API_URL)
+
 export const api: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL!,
 })

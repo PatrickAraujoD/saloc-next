8f5968a0087307e23082f2e2259195e93651c15b
PatrickAraujoD <lordpatrick200@gmail.com>
2024-09-25 14:27:14 -0300

    refactor: remove console.log

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,8 @@
 import axios, { AxiosInstance } from 'axios'
 import { config } from 'dotenv'
+import { getServerSession } from 'next-auth'
 
 config()
-
-console.log(process.env.NEXT_PUBLIC_API_URL)
 
 export const api: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL!,

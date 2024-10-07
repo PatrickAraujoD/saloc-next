e69c483f963546fc6cfc8bce6e18832e2293874f
PatrickAraujoD <lordpatrick200@gmail.com>
2024-10-06 23:40:38 -0300

    refactor: align center checkbox

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,19 +1,25 @@
 import { InputHTMLAttributes } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  nameLabel: string
+  nameLabel?: string
+  typeInput?: string
 }
 
-export function Input({ nameLabel, name, ...input }: InputProps) {
+export function Input({ nameLabel, name, typeInput, ...input }: InputProps) {
   return (
-    <div className="mt-4 space-y-1 w-full">
-      <label htmlFor={name} className="uppercase font-semibold text-blue-950">
+    <div
+      className={`${typeInput === 'checkbox' ? '' : 'space-y-1 mt-4 w-full text-start'} flex flex-col items-center w-full`}
+    >
+      <label
+        htmlFor={name}
+        className={`uppercase font-semibold text-blue-950 ${typeInput === 'checkbox' ? 'hidden' : 'text-start w-full'}`}
+      >
         {nameLabel}:
       </label>
       <input
         name={name}
         id={name}
-        className="w-full bg-transparent border-2 border-blue-950 pl-2 py-2 rounded-md"
+        className={`${typeInput === 'checkbox' ? 'h-4' : 'bg-transparent border-2 border-blue-950 pl-2 py-2 rounded-md'} w-full`}
         {...input}
       />
     </div>
